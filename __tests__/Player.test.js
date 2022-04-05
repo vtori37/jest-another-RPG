967a9bc0624d94887c7fdf7153ae473ed1ba1214
Tori Acer <vrice360@gmail.com>
2022-04-05 02:11:26 -0500

    added player-object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,8 +1,7 @@
-const Potion = require('../lib/Potion');
+const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
 
 jest.mock('../lib/Potion.js');
-
-console.log(new Potion());
 test('creates a player object', () => {
   const player = new Player('Dave');
 
@@ -13,4 +12,23 @@ test('creates a player object', () => {
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
+});
+
+test("gets player's stats as an object", () => {
+  const player = new Player('Dave');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,3 +1,5 @@
+const Potion = require('../lib/Potion.js');
+
 test('creates a health potion object', () => {
   const potion = new Potion('health');
 
